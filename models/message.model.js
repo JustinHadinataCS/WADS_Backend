3cebcfae053a3ddacf85c742e0b74f8a44d02f6a
Justin Hadinata <justinhadinata.23@gmail.com>
2025-05-06 22:18:12 +0700

    Change the MessageSchema

--- models/message.model.js
+++ models/message.model.js
@@ -4,10 +4,14 @@ const { Schema } = mongoose;
 const MessageSchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User" },
-    receiver: { type: Schema.Types.ObjectId, ref: "User" },
+    receiver: { type: Schema.Types.ObjectId, ref: "User", default: null },
+    roomId: { type: String, default: null },
+    type: { type: String, enum: ["public", "private"], required: true },
     content: { type: String, required: true },
     isRead: { type: Boolean, default: false },
     link: { type: String, default: "" },
+    isSystem: { type: Boolean, default: false },
+    attachments: [{ type: String }],
   },
   { timestamps: true }
 );
