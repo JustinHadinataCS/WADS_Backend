1b18521242dc939a8d79d2483d83505df309e9e2
Adrian <adriannugrohobasuki@gmail.com>
2025-05-09 14:54:37 +0700

    Testing feedback using authentication (Using Jest)

--- routes/ticket.route.js
+++ routes/ticket.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createTicket, deleteTicket, getTicket, getTickets, updateTicket, searchAndFilterTickets } from "../controllers/ticket.controller.js";
-import { protect } from "../middleware/auth.js";
+import { protect, user } from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.use(protect)
 router.get("/search", searchAndFilterTickets);
 router.get("/", getTickets);
 router.get("/:id", getTicket);
-router.post("/", createTicket);
+router.post("/", user, createTicket);
 router.put("/:id", updateTicket);
 router.delete("/:id", deleteTicket);
 
