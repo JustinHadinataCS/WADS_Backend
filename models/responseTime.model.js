8c8e62bb267ec0927df0062cb554859b7a7c3ded
Adrian <adriannugrohobasuki@gmail.com>
2025-05-30 18:26:05 +0700

    Response time logging data expires after 30 hours

--- models/responseTime.model.js
+++ models/responseTime.model.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
 const responseTimeSchema = new mongoose.Schema({
-  timestamp: { type: Date, required: true },
+  timestamp: { 
+    type: Date, 
+    required: true,
+    default: Date.now,
+    expires: 108000
+  },
   durationMs: { type: Number, required: true }
 });
 
